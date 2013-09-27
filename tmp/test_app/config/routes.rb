864a29f3af17c11fac13f6c77c432b608042d21f
Robert Wachs <robert.wachs@infopark.de>
2013-09-27 14:55:15 +0200

    generated test app

--- tmp/test_app/config/routes.rb
+++ tmp/test_app/config/routes.rb
@@ -1,10 +1,13 @@
 TestApp::Application.routes.draw do
   get 'sitemap.xml', controller: 'sitemap', action: 'index', format: 'xml'
-  get 'mediabrowser', to: 'mediabrowser#index'
-  get 'mediabrowser/edit', to: 'mediabrowser#edit'
-  get 'mediabrowser/modal', to: 'mediabrowser#modal'
 
   mount InfoparkDashboard::Engine => "/cms/dashboard" if Rails.env.development?
+
+  get 'mediabrowser/modal', to: 'mediabrowser#modal'
+
+  get 'mediabrowser/edit', to: 'mediabrowser#edit'
+
+  get 'mediabrowser', to: 'mediabrowser#index'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
