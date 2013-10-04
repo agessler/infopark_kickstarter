999e612d02ba27cc838afce78b268b52b1b10842
Alexander Schrot <alexander.schrot@infopark.de>
2013-10-04 13:54:55 +0200

    add example for vimeo widget

---
+++ lib/generators/cms/widget/vimeo/templates/example_migration.rb
@@ -0,0 +1,31 @@
+class CreateVimeoWidgetExample < RailsConnector::Migration
+  def up
+    homepage = Obj.find_by_path("<%= example_cms_path %>")
+
+    add_widget(homepage, "<%= example_widget_attribute %>", {
+      _obj_class: "<%= obj_class_name %>",
+      source: [{
+        url: 'http://vimeo.com/5545659'
+      }],
+    })
+  end
+
+  private
+
+  def add_widget(obj, attribute, widget)
+    widget.reverse_merge!({
+      _path: "_widgets/#{obj.id}/#{SecureRandom.hex(8)}",
+    })
+
+    widget = create_obj(widget)
+
+    revision_id = RailsConnector::Workspace.current.revision_id
+    definition = RailsConnector::CmsRestApi.get("revisions/#{revision_id}/objs/#{obj.id}")
+
+    widgets = definition[attribute] || {}
+    widgets['layout'] ||= []
+    widgets['layout'] << { widget: widget['id'] }
+
+    update_obj(definition['id'], attribute => widgets)
+  end
+end
