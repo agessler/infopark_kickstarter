a86bb631324630306da01355c1dd0d77743e29c3
Alexander Schrot <alexander.schrot@infopark.de>
2013-10-11 17:03:53 +0200

    fix youtube-widget test

--- spec/generators/widget/youtube_generator_spec.rb
+++ spec/generators/widget/youtube_generator_spec.rb
@@ -38,8 +38,8 @@ describe Cms::Generators::Widget::YoutubeGenerator do
         directory 'models' do
           file 'youtube_widget.rb' do
             contains 'cms_attribute :source, type: :linklist, max_size: 1'
-            contains 'cms_attribute :width, type: :integer'
-            contains 'cms_attribute :height, type: :integer'
+            contains 'cms_attribute :max_width, type: :integer'
+            contains 'cms_attribute :max_height, type: :integer'
             contains 'include Widget'
           end
         end
