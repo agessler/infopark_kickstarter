ac1c7845d89a19460a35861606c2519f27b99b4b
Falk Köppe <falk.koeppe@infopark.de>
2013-10-13 18:53:47 +0200

    use example_migration from own template folder

--- lib/generators/cms/widget/video/example/example_generator.rb
+++ lib/generators/cms/widget/video/example/example_generator.rb
@@ -5,7 +5,7 @@ module Cms
         class ExampleGenerator < Cms::Generators::Widget::Example::Base
           include Migration
 
-          source_root File.expand_path('../../templates', __FILE__)
+          source_root File.expand_path('../templates', __FILE__)
 
           def create_example
             example_migration_template(obj_class_name.underscore)
