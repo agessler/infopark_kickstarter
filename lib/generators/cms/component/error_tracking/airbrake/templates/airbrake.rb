a4daf500f51ac365b262c2c87729bfa91f9ca7ff
Falk Köppe <falk.koeppe@infopark.de>
2013-10-15 11:16:48 +0200

    use environment variables or configuration files for setup

--- lib/generators/cms/component/error_tracking/airbrake/templates/airbrake.rb
+++ lib/generators/cms/component/error_tracking/airbrake/templates/airbrake.rb
@@ -1,6 +1,11 @@
-configuration = YAML.load_file(Rails.root + 'config/custom_cloud.yml')
+def airbrake_config
+  config = YAML.load_file(Rails.root + 'config/custom_cloud.yml')
+  config['airbrake'] || {}
+rescue Errno::ENOENT
+  {}
+end
 
 Airbrake.configure do |config|
-  config.api_key = configuration['airbrake']['api_key']
+  config.api_key = ENV['AIRBRAKE_API_KEY'] || airbrake_config['api_key']
   config.secure = true
 end
